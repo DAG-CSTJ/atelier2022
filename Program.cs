3bd3e2fb92fd1ee062ef4913b999f50aa7c621f8
amart <amart@L110-ST-09385>
2022-01-27 14:01:41 -0500

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace atelier2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Allo World!");
+        }
+    }
+}
